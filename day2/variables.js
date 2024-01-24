1632ff67b5db203f1724006effaac05f90267c71
aishemporium <ibrahimaishat255@gmail.com>
2024-01-24 12:42:02 +0100

    first commit

---
+++ day2/variables.js
@@ -0,0 +1,46 @@
+    // Answer to Exercise 1
+let challenge = " 30 Days of JavaScript ";
+    // No2
+    console.log(challenge);
+    // No3
+    console.log(challenge.length);
+        // No4
+    console.log(challenge.toUpperCase());
+        // No5
+    console.log(challenge.toLowerCase());
+    // No6
+    console.log(challenge.substr(3));
+    // No7   
+    console.log(challenge.substring(0,2));
+    // No8
+    console.log(challenge.includes('Script'));
+    //No9
+    console.log(challenge.split());
+    //No10
+    console.log(challenge.split(' '));
+    //No11
+    let media = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+    console.log(media.split(' '));
+    //No12
+    console.log(challenge.replace('JavaScript', 'Python'));
+    //No13
+    console.log(challenge.charAt(15));
+    //No14
+    console.log(challenge.charCodeAt('j'));
+    //No15
+    console.log(challenge.indexOf('a'));
+    //No16
+    console.log(challenge.lastIndexOf('a'));
+    //No17
+    let sentence = "You cannot end a sentence with because because because is a conjunction";
+    console.log(sentence.indexOf('because'));
+    //No18
+    console.log(sentence.lastIndexOf('because'));
+    //No19
+    console.log(sentence.search('because'));
+    console.log(challenge.trim());
+    console.log(challenge.startsWith(' '));
+    console.log(challenge.endsWith(' '));
+    console.log(challenge.match('a'));
+    console.log(challenge.concat("30 Days of", "JavaScript"));
+    console.log(challenge.repeat(2));
